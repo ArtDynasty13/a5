eede5170f5452f90041313d2350c48883ca86028
root <root@DESKTOP-TJILNLP>
2025-04-09 16:33:06 -0600

    Initial commit

---
+++ fun.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "1.Functional programming ideas and how to write map in C" << std::endl;
+    std::cout << "2.The big 5 in C++ and how to reason about client programmers and invariants" << std::endl;
+    std::cout << "3.A string is NOT an array of characters ~ thank you Professor Hackman for the semester :)" << std::endl;
+    return 0;
+}
